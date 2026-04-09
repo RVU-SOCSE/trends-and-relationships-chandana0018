ff85264f112bc004077a0688d44482b2aded9f1b
chandana0018 <chandanah1bca25@rvu.edu.in>
2026-04-09 20:10:37 +0530

    Add files via upload

---
+++ lab16.py
@@ -0,0 +1,13 @@
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_csv(r"C:/Users/keert/OneDrive/Documents/lab13.csv")
+plt.plot(df['YearsExperience'],df['Salary'])
+plt.ylabel('YearsExperience')
+plt.xlabel('Index')
+plt.legend()
+plt.show()
+sns.lineplot(y=df['YearsExperience'],x=df['Salary'])
+plt.show()
